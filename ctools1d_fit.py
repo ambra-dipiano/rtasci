7d27828fad9d5f963e59b6b09788219c112a6548
ambra-dipiano <ambra.dipiano@gmail.com>
2020-12-29 15:47:52 +0100

    use_model_bkg is True

--- ctools1d_fit.py
+++ ctools1d_fit.py
@@ -48,7 +48,7 @@ analysis.model = model
 analysis.src_name = 'Crab'
 analysis.target = [target[0][0], target[1][0]]
 analysis.output = onoff_obs
-analysis.run_onoff(prefix=onoff_obs.replace('.xml',''), use_model_bkg=False)
+analysis.run_onoff(prefix=onoff_obs.replace('.xml',''))
 print(f'Onoff: {time.time() - t} s\n')
 
 # onoff
