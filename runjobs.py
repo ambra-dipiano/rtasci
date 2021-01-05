75257236c3823add896645c275c601033db50395
ambra-dipiano <ambra.dipiano@gmail.com>
2021-01-05 18:57:42 +0100

    run all jobs

---
+++ runjobs.py
@@ -0,0 +1,9 @@
+from os import listdir, system
+from os.path import isfile, join
+
+path = '/home/ambra/Desktop/CTA/projects/cta-sag-sci/jobs'
+
+jobs = [f for f in listdir(path) if '_rubjobs.sh' in f and isfile(join(path, f))]
+
+for job in jobs:
+    os.system('sbatch job')
