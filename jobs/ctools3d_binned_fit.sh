7ca3490407988dc87951deebe8fa03b35f9d7b12
ambra-dipiano <ambra.dipiano@gmail.com>
2021-01-05 19:34:33 +0100

    mod .gitignore and add .sh

---
+++ jobs/ctools3d_binned_fit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+conda activate ctools
+	python3 /home/ambra/Desktop/CTA/projects/cta-sag-sci/jobs/ctools3d_binned_fit.py 1000 True
+	python3 /home/ambra/Desktop/CTA/projects/cta-sag-sci/jobs/ctools3d_binned_fit.py 10 False
+	python3 /home/ambra/Desktop/CTA/projects/cta-sag-sci/jobs/ctools3d_binned_fit.py 1000 False
+	python3 /home/ambra/Desktop/CTA/projects/cta-sag-sci/jobs/ctools3d_binned_fit.py 10 False
+	python3 /home/ambra/Desktop/CTA/projects/cta-sag-sci/jobs/ctools3d_binned_fit.py 1000 False
+	python3 /home/ambra/Desktop/CTA/projects/cta-sag-sci/jobs/ctools3d_binned_fit.py 10 False
+	python3 /home/ambra/Desktop/CTA/projects/cta-sag-sci/jobs/ctools3d_binned_fit.py 1000 False
+	python3 /home/ambra/Desktop/CTA/projects/cta-sag-sci/jobs/ctools3d_binned_fit.py 10 False
+	python3 /home/ambra/Desktop/CTA/projects/cta-sag-sci/jobs/ctools3d_binned_fit.py 1000 False
+	python3 /home/ambra/Desktop/CTA/projects/cta-sag-sci/jobs/ctools3d_binned_fit.py 10 False
+	python3 /home/ambra/Desktop/CTA/projects/cta-sag-sci/jobs/ctools3d_binned_fit.py 1000 False
+	python3 /home/ambra/Desktop/CTA/projects/cta-sag-sci/jobs/ctools3d_binned_fit.py 10 False
+	python3 /home/ambra/Desktop/CTA/projects/cta-sag-sci/jobs/ctools3d_binned_fit.py 1000 False
+	python3 /home/ambra/Desktop/CTA/projects/cta-sag-sci/jobs/ctools3d_binned_fit.py 10 False
+	python3 /home/ambra/Desktop/CTA/projects/cta-sag-sci/jobs/ctools3d_binned_fit.py 1000 False
+	python3 /home/ambra/Desktop/CTA/projects/cta-sag-sci/jobs/ctools3d_binned_fit.py 10 False
+	python3 /home/ambra/Desktop/CTA/projects/cta-sag-sci/jobs/ctools3d_binned_fit.py 1000 False
+	python3 /home/ambra/Desktop/CTA/projects/cta-sag-sci/jobs/ctools3d_binned_fit.py 10 False
+	python3 /home/ambra/Desktop/CTA/projects/cta-sag-sci/jobs/ctools3d_binned_fit.py 1000 False
+	python3 /home/ambra/Desktop/CTA/projects/cta-sag-sci/jobs/ctools3d_binned_fit.py 10 False
