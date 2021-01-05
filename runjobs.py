8e0c46f2898cd6aa71c508a7564343baba7d8e86
ambra-dipiano <ambra.dipiano@gmail.com>
2021-01-05 19:24:14 +0100

    fixed job submission script

--- runjobs.py
+++ runjobs.py
@@ -1,9 +1,10 @@
 from os import listdir, system
 from os.path import isfile, join
 
-path = '/home/ambra/Desktop/CTA/projects/cta-sag-sci/jobs'
+path = '/home/ambra/Desktop/CTA/projects/cta-sag-sci/jobs/'
 
-jobs = [f for f in listdir(path) if '_rubjobs.sh' in f and isfile(join(path, f))]
+jobs = [f for f in listdir(path) if '_runjobs.sh' in f and isfile(join(path, f))]
 
 for job in jobs:
-    os.system('sbatch job')
+    #os.system('sbatch job')
+    print(f'sbatch {job}')
