69c096e19fd3d6d568a803521462cce49f486327
ambra-dipiano <ambra.dipiano@inaf.it>
2021-01-13 15:53:42 +0100

    conda activate changed to source activate

--- jobs/ctools3d_binned_fit.sh
+++ jobs/ctools3d_binned_fit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-conda activate scitools
+source activate scitools
 	python3 /home/ambra/Desktop/CTA/projects/cta-sag-sci/jobs/ctools3d_binned_fit.py 1000 True
 	python3 /home/ambra/Desktop/CTA/projects/cta-sag-sci/jobs/ctools3d_binned_fit.py 100 False
 	python3 /home/ambra/Desktop/CTA/projects/cta-sag-sci/jobs/ctools3d_binned_fit.py 10 False
--- make_sh.py
+++ make_sh.py
@@ -10,7 +10,7 @@ shname = f'{path}/{script.replace(".py", ".sh")}'
 shrun = f'{path}/{script.replace(".py", "_runjobs.sh")}'
 sh = open(shname, 'w+')
 sh.write('#!/bin/bash\n')
-sh.write('\nconda activate scitools')
+sh.write('\nsource activate scitools')
 
 for i in range(N):
     if i == 0:
