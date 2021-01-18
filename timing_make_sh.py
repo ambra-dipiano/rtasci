e6235748110cf9320ac59ce06d878667fdcc2bbb
ambra-dipiano <ambra.dipiano@inaf.it>
2021-01-18 10:37:45 +0100

    update make_sh and remaned for timing only

--- timing_make_sh.py
+++ timing_make_sh.py
@@ -11,28 +11,29 @@ else:
     N = 10
 
 rootpath = str(os.path.dirname(os.path.abspath(__file__)))
-path = f'{rootpath}/jobs'
-shjob = f'{path}/jobs_all.sh'
-shrun = f'{path}/runjobs_all.sh'
-scripts = [f for f in listdir(rootpath) if isfile(join(rootpath, f)) and ('ctools' in f or 'gammapy' in f) and ('cumulative' not in f and 'lima' not in f)]
+pypath = f'{rootpath}/timing'
+jobpath = f'{rootpath}/jobs'
+shjob = f'{jobpath}/jobs_all.sh'
+shrun = f'{jobpath}/runjobs_all.sh'
+scripts = [f for f in listdir(pypath) if isfile(join(pypath, f)) and ('ctools' in f or 'gammapy' in f) and ('cumulative' not in f and 'lima' not in f)]
 
 print(scripts)
 
 for script in scripts:
-    shname = f'{path}/{script.replace(".py", ".sh")}'
+    shname = f'{jobpath}/{script.replace(".py", ".sh")}'
     sh = open(shname, 'w+')
     sh.write('#!/bin/bash\n')
     sh.write('\nsource activate scitools')
 
     for i in range(N):
         if i == 0:
-            sh.write(f'\n\tpython3 {rootpath}/timing/{script} 1000 {True}')
-            sh.write(f'\n\tpython3 {rootpath}/timing/{script} 100 {False}')
-            sh.write(f'\n\tpython3 {rootpath}/timing/{script} 10 {False}')
+            sh.write(f'\n\tpython3 {pypath}/{script} 1000 {True}')
+            sh.write(f'\n\tpython3 {pypath}/{script} 100 {False}')
+            sh.write(f'\n\tpython3 {pypath}/{script} 10 {False}')
         else:
-            sh.write(f'\n\tpython3 {rootpath}/timing/{script} 1000 {False}')
-            sh.write(f'\n\tpython3 {rootpath}/timing/{script} 100 {False}')
-            sh.write(f'\n\tpython3 {rootpath}/timing/{script} 10 {False}')
+            sh.write(f'\n\tpython3 {pypath}/{script} 1000 {False}')
+            sh.write(f'\n\tpython3 {pypath}/{script} 100 {False}')
+            sh.write(f'\n\tpython3 {pypath}/{script} 10 {False}')
 
     sh.write('\n')
     sh.close()
@@ -41,7 +42,7 @@ sh = open(shjob, 'w+')
 sh.write('#!/bin/bash\n')
 for script in scripts:
     sh.write(f'echo {script.replace(".py", ".sh")}\n')
-    sh.write(f'bash {path}/{script.replace(".py", ".sh")}\n')
+    sh.write(f'bash {jobpath}/{script.replace(".py", ".sh")}\n')
 
 sh = open(shrun, 'w+')
 sh.write('#!/bin/bash\n')
