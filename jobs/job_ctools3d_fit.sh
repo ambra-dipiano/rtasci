a208e4cc8f99edbaa3f225865ff3c28e696ea470
ambra-dipiano <ambra.dipiano@gmail.com>
2021-01-26 12:20:26 +0100

    removed redountant jobs

--- jobs/job_ctools3d_fit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#SBATCH --job-name=ambra
-#SBATCH --output=slurm-/home/ambra/Desktop/CTA/projects/cta-sag-sci/jobs/job_ctools3d_fit.sh.out
-#SBATCH --account=ambra
-#SBATCH --nodes=1
-#SBATCH --cpus-per-task=1
-
-exec sh /home/ambra/Desktop/CTA/projects/cta-sag-sci/jobs/ctools3d_fit.sh
