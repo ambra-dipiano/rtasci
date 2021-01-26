3c282782c66b5f32d2abc9ee75779247161a2682
ambra-dipiano <ambra.dipiano@gmail.com>
2021-01-26 14:22:01 +0100

    sci and time hdrs

--- compareCtoolsGammapy.py
+++ compareCtoolsGammapy.py
@@ -1,4 +1,6 @@
 import os
+import pandas as pd
+import numpy as np
 from lib.RTAVisualise import *
 from os.path import join, isdir, isfile
 from os import listdir
@@ -10,8 +12,24 @@ if not isdir(pngpath):
     print('Creating png folder...')
     os.mkdir(pngpath)
 
-tables = [f for f in listdir(datapath) if '.csv' in f]
-print(len(tables))
+tables = [f for f in listdir(datapath) if 'ctools1d_binned' in f]
+print(f'Tables: {len(tables)}\n')
 
-for table in tables:
-    data = pd.read_csv(join(datapath, table), sep=' ', )
+for i, table in enumerate(tables):
+    print(f'Collecting data from {table}')
+    data = pd.read_csv(join(datapath, table), sep=' ')
+    print(data[:5])
+
+    d1000 = data[data['texp'] == 1000]
+    d100 = data[data['texp'] == 100]
+    d10 = data[data['texp'] == 10]
+
+    m1000 = d1000.mean()
+    print(m1000.keys())
+    print(m1000[m1000.keys()[0]])
+
+    sci = ['texp', 'sqrt_ts', 'flux', 'flux_err']
+    time = ['ttotal', 'timport', 'tsetup', 'tobs', 'tconf', 'tred', 'tblind', 'tcube', 'texpcube', 'tpsfcube', 'bkgcube', 'tmodel', 'tonoff', 'tfit', 'tstat', 'tflux']
+
+    hdr_sci = ''
+    hdr_t = ''
