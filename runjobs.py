7305ebbc6ece79e5c161176cc60228bdd900c9d1
ambra-dipiano <ambra.dipiano@inaf.it>
2021-01-26 19:22:41 +0100

    updated script to run jobs as bash

--- runjobs.py
+++ runjobs.py
@@ -5,8 +5,10 @@ from os.path import isfile, join
 rootpath = str(os.path.dirname(os.path.abspath(__file__))).replace('cta-sag-sci', '')
 path = f'{rootpath}/cta-sag-sci/jobs/'
 
-jobs = [f for f in listdir(path) if '_runjobs.sh' in f and isfile(join(path, f))]
+jobs = [f for f in listdir(path) if 'job_' not in f and isfile(join(path, f))]
 
 for job in jobs:
-    print(f'sbatch jobs/{job}')
+    print(f'bash jobs/{job}')
     os.system(f'bash jobs/{job}')
+
+print(len(jobs))
