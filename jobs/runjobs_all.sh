f1e71195dd3e9bf55708e37c7465330b3fb720b3
ambra-dipiano <ambra.dipiano@inaf.it>
2021-01-26 12:16:28 +0100

    rm launching of all jobs in one

--- jobs/runjobs_all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#SBATCH --job-name=ambra
-#SBATCH --account=ambra
-#SBATCH --nodes=1
-#SBATCH --cpus-per-task=1
-
-exec sh /data01/homes/cta/gammapy_integration/cta-sag-sci/jobs/jobs_all.sh
