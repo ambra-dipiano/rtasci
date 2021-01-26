a0595922bcb775f5a2699aa6a1f5589b7e02e8af
ambra-dipiano <ambra.dipiano@inaf.it>
2021-01-26 12:25:12 +0100

    rm redoundant bash

--- jobs/job_ctools1d_fit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#SBATCH --job-name=ambra
-#SBATCH --output=slurm-/home/ambra/Desktop/CTA/projects/cta-sag-sci/jobs/job_ctools1d_fit.sh.out
-#SBATCH --account=ambra
-#SBATCH --nodes=1
-#SBATCH --cpus-per-task=1
-
-exec sh /home/ambra/Desktop/CTA/projects/cta-sag-sci/jobs/ctools1d_fit.sh
