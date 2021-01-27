b964f9cc9d94f524e210a411873a7c352216c7fb
ambra-dipiano <ambra.dipiano@inaf.it>
2021-01-27 19:02:19 +0100

    main pipeline; it loads a yaml config and runs the templates/models preparation script for a given catalog

--- rtactools.py
+++ rtactools.py
@@ -5,8 +5,12 @@ from os.path import join
 
 # configure
 cfgfile = sys.argv[1]
+pypath = str(os.path.dirname(os.path.abspath(__file__)))  
+configuration = open(os.path.join(pypath, cfgfile))
+cfg = yaml.load(configuration, Loader=yaml.FullLoader)
 
-print('Preparing the catalog...\n')
-os.system(f'python3 simGRBpreparation.py {cfgfile}')
+if cfg['options']['extract_data']:
+    print('Preparing the catalog...\n')
+    os.system(f'python3 simGRBpreparation.py {cfgfile}')
 
 print('\n\nExit\n\n')
