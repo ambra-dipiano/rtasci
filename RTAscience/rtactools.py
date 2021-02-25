9dbfe48e669b5b43ca82066f1c3c164f3558c95b
ambra-dipiano <ambra.dipiano@inaf.it>
2021-02-25 15:18:27 +0100

    rename and add line parameter to keep or delate fits and xml files produced during the analysis and simulation

--- RTAscience/rtactools.py
+++ RTAscience/rtactools.py
@@ -8,6 +8,7 @@ from RTAscience.cfg.Config import Config
 # configure
 parser = argparse.ArgumentParser(description='ADD SCRIPT DESCRIPTION HERE')
 parser.add_argument('-f', '--cfgfile', type=str, required=True, help="Path to the yaml configuration file")
+parser.add_argument('--remove', type=str, default='true', help='Keep only outputs')
 args = parser.parse_args()
 
 cfg = Config(args.cfgfile)
@@ -16,9 +17,9 @@ cfg = Config(args.cfgfile)
 if cfg.get('simtype').lower() == 'grb':
     if cfg.get('extract_data'):
         print('\nPreparing GRB catalog...\n')
-        os.system(f'python3 simGRBpreparation.py -f {args.cfgfile}')
+        os.system(f'python3 prepareGRBcatalog.py -f {args.cfgfile}')
     print('\nRun simulations...\n')
-    os.system(f'python3 simGRBcat.py -f {args.cfgfile}')
+    os.system(f'python3 simGRBcatalog.py -f {args.cfgfile}')
 elif cfg.get('simtype').lower() == 'bkg':
     print('\nComputing BKG-ONLY simulations is work in progress\n')
 elif cfg.get('simtype').lower() == 'skip':
@@ -40,6 +41,6 @@ elif cfg.get('tool') == 'ctools':
         pipeline += 'blind'
     pipeline += 'fit.py'
     print(f'Pipeline: {pipeline}')
-    os.system(f"python3 pipelines/{pipeline} -f {args.cfgfile}")
+    os.system(f"python3 pipelines/{pipeline} -f {args.cfgfile} --remove {args.remove.lower()}")
 
 print('\n\nExit\n\n')
