ee986ea777219ce40b5eec825a321cc33ad7ac8d
ambra-dipiano <ambra.dipiano@inaf.it>
2021-03-15 14:37:45 +0100

    accept caldb parameter from command line

--- RTAscience/degradation_caldb.py
+++ RTAscience/degradation_caldb.py
@@ -7,14 +7,17 @@
 # Ambra Di Piano <ambra.dipiano@inaf.it>
 # *******************************************************************************
 
-from RTAscience.lib.RTAIrfs import RTAIrfs
 import os
+import argparse
+from RTAscience.lib.RTAIrfs import RTAIrfs
 
 # files ---!
-caldb = ['prod3b', 'prod3b-v2']
+parser = argparse.ArgumentParser()
+parser.add_argument("--caldb", nargs="+", default=['prod3b', 'prod3b-v2'])
+args = parser.parse_args()
 
 # initialise ---!
-for db in caldb:
+for db in args.caldb:
   print(f'processing {db} degradation')
   irf = os.listdir(os.environ.get('CTOOLS') + '/share/caldb/data/cta/' + db + '/bcf/')
   for fits in irf:
