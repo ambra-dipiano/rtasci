8caf87a2efd169b1488fc12526db63a1ed171c31
ambra-dipiano <ambra.dipiano@inaf.it>
2021-03-17 11:45:32 +0100

    check that scalefluxfactor is set correctly in config

--- RTAscience/prepareGRBcatalog.py
+++ RTAscience/prepareGRBcatalog.py
@@ -33,6 +33,10 @@ else:
     runids = cfg.get('runid')
 runids = sorted(runids)
 
+# check scalefluxfactor ---!
+if type(cfg.get('scalefluxfactor')) != float or type(cfg.get('scalefluxfactor')) != int:
+    raise ValueError('The parameter "scalefluxfactor" must be int or float. If you want to use the nominal template, please set scalefluxfactor=1.')
+
 # conditions control ---!
 set_ebl = cfg.get('set_ebl')  # uses the EBL absorbed template
 # paths ---!
