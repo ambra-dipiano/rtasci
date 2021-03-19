cb842ec1a9875c011bd41adc2717d751707f3d7c
ambra-dipiano <ambra.dipiano@inaf.it>
2021-03-19 12:04:46 +0100

    shift template by adding onset value to time grid

--- RTAscience/simGRBcatalog.py
+++ RTAscience/simGRBcatalog.py
@@ -98,36 +98,28 @@ for runid in runids:
         tgrid, tbin_start, tbin_stop = sim.getTimeSlices(GTI=(cfg.get('delay'), tmax), return_bins=True) 
 
         # -------------------------------------------------------- simulate ---!!!
-        print('Simulate Bkg+GRB with template')
+        print(f'Simulate seed={count}')
         for j in range(tbin_stop-tbin_start-1):
-            sim.t = [tgrid[j], tgrid[j + 1]]
-            print(f'GTI (bin) = {sim.t}')
+            sim.t = [tgrid[j]+cfg.get('onset'), tgrid[j + 1]+cfg.get('onset')]
+            if args.print.lower() == 'true':
+                print(f'GTI (bin) = {sim.t}')
             sim.model = join(datapath, f'extracted_data/{runid}/{runid}_tbin{tbin_start+j:02d}.xml')
             event = join(grbpath, f'{name}_tbin{tbin_start+j:02d}.fits')
             event_bins.append(event)
             sim.output = event
             sim.run_simulation()
-            if args.print.lower() == 'true':
-                h = fits.open(event)
-                print('Check GTI and EVENTS time range:')
-                print('----------')
-                print(h[2].data)
-                print(h[1].data.field('TIME').min(), h[1].data.field('TIME').max())
-                print('----------')
-                h.close()
+
         # -------------------------------------------- shift time --- !!!
         if cfg.get('onset') != 0:
             if cfg.get('delay') != 0:
                 raise ValueError('Bad configuration. Either "onset" or "delay" must be equal to 0.')
-            onset = cfg.get('onset')
-            print(f'Shift template of {onset} s after the start of the observation')
-            sim.shiftTemplateTime(phlist=event_bins, time_shift=onset)
-
             # ------------------------------------ add background --- !!!
             print('Simulate bkg to append before the burst')
             bkg = os.path.join(grbpath, f'bkg{count:06d}.fits')
             event_bins.insert(0, bkg)
-            sim.t = [0, onset]
+            sim.t = [0, cfg.get('onset')]
+            if args.print.lower() == 'true':
+                print(f'GTI (bkg) = {sim.t}')
             sim.model = bkg_model
             sim.output = bkg
             sim.run_simulation()
@@ -156,6 +148,9 @@ for runid in runids:
             if os.path.isfile(obslist):
                 os.remove(obslist)
             make_obslist(obslist=obslist, items=event_bins, names=name)
+
+
+
 
         del sim
         if args.remove.lower() == 'true' and args.merge.lower() == 'true':
