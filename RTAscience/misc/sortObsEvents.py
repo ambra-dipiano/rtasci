ebebd4ea1fcd2fbe0ca948c82be28ea89dd7facb
Ambra Di Piano <ambra.dipiano@inaf.it>
2021-09-20 09:15:27 +0200

    script sortObsEvents

---
+++ RTAscience/misc/sortObsEvents.py
@@ -0,0 +1,19 @@
+# *******************************************************************************
+# Copyright (C) 2021 INAF
+#
+# This software is distributed under the terms of the BSD-3-Clause license
+#
+# Authors:
+# Ambra Di Piano <ambra.dipiano@inaf.it>
+# *******************************************************************************
+
+import sys
+import numpy as np
+from os.path import expandvars, join, abspath
+from RTAscience.lib.RTACtoolsSimulation import RTACtoolsSimulation as sim
+
+events = sim()
+events.input = expandvars(sys.argv[1])
+events.t = [0, 1000]
+events.sortObsEvents(key='TIME')
+
