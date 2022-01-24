bcba0a7949a0c0eebfdef0a1b4b298018bcb57ae
ambra-dipiano <ambra.dipiano@gmail.com>
2022-01-24 10:48:41 +0100

    add overlay option

--- RTAscience/misc/pvalues.py
+++ RTAscience/misc/pvalues.py
@@ -30,14 +30,15 @@ df.sort_values(cols[0], inplace=True)
 values = np.array(df[cols[0]])
 values.sort()
 
-values = np.genfromtxt(join(path, filename), usecols=(0), skip_header=0, dtype=float)
+#values = np.genfromtxt(join(path, filename), usecols=(0), skip_header=0, dtype=float)
 
 nbin = 100
+print(min(values), max(values), len(values))
 
 # -------------------------------- PLOT ---!
 
-fig, ax = ts_wilks(values, len(values), nbin=nbin, figsize=(10, 6), title='distribution', show=False, usetex=False, filename=png_path + filename.replace('.csv', '_wilks.png'))
+fig, ax = ts_wilks(values, len(values), nbin=nbin, figsize=(10, 6), title='distribution', show=False, usetex=False, filename=png_path + filename.replace('.csv', '_wilks.png'), overlay=None)
 
-fig, ax = p_values(values, len(values), nbin=nbin, figsize=(10, 6), title='pvalues', show=False, usetex=False, filename=png_path + filename.replace('.csv', '_pvalues.png'))
+fig, ax = p_values(values, len(values), nbin=nbin, figsize=(10, 6), title='pvalues', show=False, usetex=False, filename=png_path + filename.replace('.csv', '_pvalues.png'), overlay=None)
 
-fig, ax = ts_wilks_cumulative(values, len(values), nbin=nbin, figsize=(10, 6), show=False, usetex=False, title='cumulative', filename=png_path + filename.replace('.csv', '_cumulative.png'))
+fig, ax = ts_wilks_cumulative(values, len(values), nbin=nbin, figsize=(10, 6), show=False, usetex=False, title='cumulative', filename=png_path + filename.replace('.csv', '_cumulative.png'), overlay=None)
