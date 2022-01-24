638b52f971faa7275f4a06c38320cbfb531ce26e
ambra-dipiano <ambra.dipiano@gmail.com>
2022-01-24 15:44:53 +0100

    clean up code

--- RTAscience/misc/pvalues.py
+++ RTAscience/misc/pvalues.py
@@ -8,6 +8,7 @@
 # *******************************************************************************
 
 import os
+import pandas as pd
 from os.path import join, expandvars
 from RTAscience.lib.RTAStats import *
 
@@ -17,28 +18,25 @@ if not os.path.isdir(png_path):
   os.mkdir(png_path)
 
 
-filename = 'reconstruction_errors_2.csv'
+filename = 'reconstruction_errors_3.csv'
 os.system(f'head {filename}')
 # load DataFrame and column names ---!
 df = pd.read_csv(join(path, filename))
 cols = list(df.columns)
 trials = len(df[cols[0]])
-print('verify trials = ', trials)
 # drop duplicates ---!
 df.sort_values(cols[0], inplace=True)
 # set arrays ---!
 values = np.array(df[cols[0]])
 values.sort()
 
-#values = np.genfromtxt(join(path, filename), usecols=(0), skip_header=0, dtype=float)
-
 nbin = 100
 print(f'min = {min(values)}\nmax = {max(values)}\nlen = {len(values)}')
 
 # -------------------------------- PLOT ---!
 
-fig, ax = ts_wilks(values, len(values), nbin=nbin, figsize=(10, 6), title='distribution', show=False, usetex=False, filename=png_path + filename.replace('.csv', '_wilks.png'), overlay=None)
+fig, ax = ts_wilks(values, len(values), nbin=nbin, figsize=(7, 8), title='distribution', show=False, usetex=False, filename=png_path + filename.replace('.csv', '_wilks.png'), overlay=None, write_data=True)
 
-fig, ax = p_values(values, len(values), nbin=nbin, figsize=(10, 6), title='pvalues', show=False, usetex=False, filename=png_path + filename.replace('.csv', '_pvalues.png'), overlay=None)
+fig, ax = p_values(values, len(values), nbin=nbin, figsize=(7, 8), title='pvalues', show=False, usetex=False, filename=png_path + filename.replace('.csv', '_pvalues.png'), overlay=None, sigma5=False, write_data=True)
 
-fig, ax = ts_wilks_cumulative(values, len(values), nbin=nbin, figsize=(10, 6), show=False, usetex=False, title='cumulative', filename=png_path + filename.replace('.csv', '_cumulative.png'), overlay=None)
+#fig, ax = ts_wilks_cumulative(values, len(values), nbin=nbin, figsize=(7, 8), show=False, usetex=False, title='cumulative', filename=png_path + filename.replace('.csv', '_cumulative.png'), overlay=None)
