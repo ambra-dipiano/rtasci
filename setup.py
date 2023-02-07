9c55abe3bf2d6e18fb75193689495c81c7adddd0
Ambra Di Piano <ambra.dipiano@inaf.it>
2023-02-07 16:45:38 +0100

    packaging

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
-#!/usr/bin/python
-# -*- coding: latin-1 -*-
+# *****************************************************************************
+# Copyright (C) 2020 INAF
+# This software is distributed under the terms of the BSD-3-Clause license
+#
+# Authors:
+# Ambra Di Piano <ambra.dipiano@inaf.it>
+# *****************************************************************************
+
 from setuptools import setup, find_packages
-setup( name='RTAscience',
-       version='0.1.0',
-       author='Ambra Di Piano',
-       author_email='ambra.dipiano@inaf.it',
-       packages=find_packages(),
-       package_dir={ 'RTAscience': 'RTAscience' },
-       include_package_data=True,
-       license='BSD-3-Clause'
-     )
+
+setup(name='RTAscience',
+     author='Ambra Di Piano',
+     author_email='ambra.dipiano@inaf.it',
+     package_dir={'RTAscience': 'RTAscience'},
+    packages=find_packages(),
+    include_package_data=True,
+    license='BSD-3-Clause',
+)
