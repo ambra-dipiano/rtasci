e8d54ef17a78e01f0881ef82b68ca771f07c4186
Ambra Di Piano <ambra.dipiano@inaf.it>
2023-09-20 14:44:14 +0200

    update prod5 script

--- download_prod5.sh
+++ download_prod5.sh
@@ -4,6 +4,4 @@ mkdir tmp
 cd tmp
 wget http://cta.irap.omp.eu/ctools/_downloads/csadd2caldb.py
 wget https://zenodo.org/record/5499840/files/cta-prod5-zenodo-fitsonly-v0.1.zip
-python csadd2caldb.py debug=yes
-cd ..
-rm -rf tmp
+unzip cta-prod5-zenodo-fitsonly-v0.1.zip
